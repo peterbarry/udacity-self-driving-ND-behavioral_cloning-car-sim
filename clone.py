6210fe24068c6402c93fe24bdc06dbf772449b09
Peter J. Barry <pete.barry@gmail.com>
2017-03-30 03:56:39 +0100

    update model for simple lenet

--- clone.py
+++ clone.py
@@ -33,14 +33,38 @@ X_train =  np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+input_image_shape = (160,320,3)
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+#normalize and mean center the data in a labda layer
+model.add(Lambda(lambda x: x / 255.0 - 0.5 , input_shape=input_image_shape))
+#model.add(Flatten(input_shape=input_image_shape))
+
+model.add(Convolution2D(6,5,5,
+                 activation='relu',
+                 ))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,
+                 activation='relu',
+                 ))
+model.add(MaxPooling2D())
+#model.add(Dropout(0.25))
+model.add(Flatten())
+#model.add(Dense(128, activation='relu'))
+model.add(Dense(120))
+model.add(Dense(86))
+#model.add(Dense(128, activation='relu'))
+#model.add(Dense(num_classes, activation='softmax'))
+
+
 model.add(Dense(1))
 
 model.compile(loss='mse',optimizer='adam')
-model.fit(X_train,y_train, validation_split=0.2,shuffle=True,nb_epoch=7)
+model.fit(X_train,y_train, validation_split=0.2,shuffle=True,nb_epoch=5)
 model.save('model.h5')
 
 
